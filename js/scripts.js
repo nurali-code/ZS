9e11ed14f1484620f630aa6069b7b06977087529
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-08-27 16:18:31 +0500

    catalog

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,58 @@
 $(document).ready(function () {
     $('.btn-menu').on('click', function () {
-        $('header, .btn-menu').toggleClass('active');
+        $('header, .btn-menu, body').toggleClass('active');
     })
 
-    $("#showAll").click(function () {
-        $(this).remove();
-        $(".map .hidden, .map .mob-hidden").removeClass("hidden mob-hidden");
+    /*-------------------------------------------------end*/
+
+    $('.btn_catalog').on('click', function (e) {
+        e.preventDefault();
+        const catalog = $(this).attr("href");
+        $('.btn_catalog').removeClass('active')
+        $(this).addClass('active')
+        $('.catalog').show(0);
+        $('.catalog-content').not(catalog).hide(0);
+        $(catalog).fadeIn(300);
     });
 
-
-    /*---------------------------------------------------end*/
+    /*-------------------------------------------------end*/
 
     function hideModals() {
         $('.modal').fadeOut();
-        $('.modal, body').removeClass('active');
+        $('body').removeClass('active');
     };
 
     $(function () {
         function showModal(id) {
-            if ($(id).hasClass('active')) {
-                $(id).fadeOut(300)
-                $(id).removeClass('active');
-                $('body').removeClass('active');
-            } else {
-                $(id).addClass('active')
-                $('body').addClass('active');
-                $(id).fadeIn(300);
-            }
+            $('body').addClass('active');
+            $(id).fadeIn(300);
         }
 
         $('[data-modal]').on('click', function (e) {
-            e.preventDefault();
-            showModal('#' + $(this).attr("data-modal"));
+            e.preventDefault(); showModal('#' + $(this).attr("data-modal"));
         });
 
-        $('.modal-close').on('click', () => {
-            hideModals();
-        });
+        $('.modal-close').on('click', () => { hideModals(); });
 
         $(document).on('click', function (e) {
-            if (!(
-                ($(e.target).parents('.modal-content').length) ||
+            if (!(($(e.target).parents('.modal-content').length) ||
                 ($(e.target).parents('.nav').length) ||
                 ($(e.target).hasClass('btn')) ||
                 ($(e.target).hasClass('modal-content'))
-            )) {
-                hideModals();
-            }
+            )) { hideModals(); }
         });
     });
-
-
-
 
     /*---------------------------------------------------end*/
 
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top, }, 300,)
+        var offNum;
+        $(this).hasClass('btn_catalog') ? offNum = 200 : offNum = 0;
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - offNum, }, 300,)
         $('header, .btn-menu').removeClass('active');
     })
-    
+
     /*---------------------------------------------------end*/
 
     $(window).scroll(function () {
@@ -76,10 +68,12 @@ $(document).ready(function () {
     $('input[type="tel"]').inputmask({ "mask": "8-999-999-99-99" });
 
     /*---------------------------------------------------end*/
+
     $('.dropdown-btn').click(function () {
         $(this).toggleClass('active');
         $(this).next('.dropdown-content').slideToggle();
     });
+
     /*---------------------------------------------------end*/
 
     $("form").submit(function () {
